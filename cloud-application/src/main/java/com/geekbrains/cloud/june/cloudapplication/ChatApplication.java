e303b8190e411bd57dd49f7a512e6092babdf89a
USER <sabanashvili.mihail@yandex.ru>
2022-06-16 17:56:24 +0300

    добавил контроллер для логина

--- cloud-application/src/main/java/com/geekbrains/cloud/june/cloudapplication/ChatApplication.java
+++ cloud-application/src/main/java/com/geekbrains/cloud/june/cloudapplication/ChatApplication.java
@@ -8,10 +8,13 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class ChatApplication extends Application {
+    public static boolean isEntered;
 
     @Override
     public void start(Stage stage) throws IOException {
-        login(stage);
+        while (!isEntered) {
+            login(stage);
+        }
         stage.close();
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("file_list.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 750);
@@ -21,6 +24,7 @@ public class ChatApplication extends Application {
     }
 
     public void login(Stage stage) throws IOException {
+        isEntered = false;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 750, 750);
         stage.setTitle("Login");
---
+++ cloud-application/src/main/java/com/geekbrains/cloud/june/cloudapplication/LoginController.java
@@ -0,0 +1,87 @@
+package com.geekbrains.cloud.june.cloudapplication;
+
+import com.geekbrains.cloud.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @FXML
+    TextField loginL;
+    @FXML
+    TextField passwordL;
+    @FXML
+    TextField loginA;
+    @FXML
+    TextField passwordA;
+    @FXML
+    TextField firstName;
+    @FXML
+    TextField secondName;
+    @FXML
+    TextField email;
+    @FXML
+    TextArea error;
+
+    private Network network;
+
+    public void authenticate() {
+        try {
+            network.write(new AuthRequest(loginL.getText(), passwordL.getText()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void authorize() {
+        try {
+            network.write(new AuthorizeRequest(loginA.getText(), passwordA.getText(), firstName.getText(),
+                    secondName.getText(), email.getText()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readLoop() {
+        try {
+            while (true) {
+                CloudMessage message = network.read();
+                if(message instanceof AuthResponse response) {
+                    if(!response.isAuth())
+                        error.setText("You are not registered. Please, log in");
+                    else {
+                        ChatApplication.isEntered = true;
+                        break;
+                    }
+                } else if(message instanceof AuthorizeResponse response) {
+                    if(!response.isSuccess())
+                        error.setText("Something went wrong. Please, log in again");
+                    else {
+                        ChatApplication.isEntered = true;
+                        break;
+                    }
+                }
+
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            network = new Network(8189);
+            Thread readThread = new Thread(this::readLoop);
+            readThread.setDaemon(true);
+            readThread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
