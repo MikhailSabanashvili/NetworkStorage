a2cfb90a37185150d305ff9e9d6640cec3d8a3c3
User <sabanashvili.mihail@yandex.ru>
2022-07-16 18:34:47 +0300

    убрал лишние методы

--- cloud-server/src/main/java/com/geekbrains/cloud/netty/dao/Dao.java
+++ cloud-server/src/main/java/com/geekbrains/cloud/netty/dao/Dao.java
@@ -2,6 +2,5 @@ package com.geekbrains.cloud.netty.dao;
 
 public interface Dao {
     void insertUser(String login, String password, String firstName, String secondName, String email);
-    boolean isExist(String login, String password);
-    String getId(String login);
+    User getUser(String login);
 }
